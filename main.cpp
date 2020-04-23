0972309d77175b42c4ca45d1bb3f66d540668bfc
root <root@localhost.localdomain>
2020-04-24 02:35:47 +0800

    socket封装

--- main.cpp
+++ main.cpp
@@ -1,20 +1,59 @@
-#include "util.h"
-#include "epoll.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <vector>
 #include <queue>
+#include <string.h> //bzero
+#include <arpa/inet.h> //htonl..
 #include <sys/epoll.h>
-
+#include <sys/socket.h>
+#include "util.h"
+#include "epoll.h"
 
 using namespace std;
 
 const int PORT = 8888;
 
-int socket_bind_listen(int port){
 
+//监听socket的初始化
+int socket_bind_listen(int port){
+    
+    //检查port是否合法
+    if(port < 1024 || port > 65535){
+        return -1;
+    }
+    int listen_fd = 0;
+    if((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        return -1;
+    }
+    
+    int optval = 1;
+
+    //设置地址复用,消除bind时服务器处于time_wait状态无法连接
+    //"Address already in use"
+    if(setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1){
+        return -1;
+    }
+
+    struct sockaddr_in server_addr;
+    bzero((char*)&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons((unsigned short)port);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if(bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1){
+        return -1;
+    }
+
+    if(listen(listen_fd, LISTENQ) == -1){
+        return -1;
+    }
+    //监听描述符非法
+    if(listen_fd == -1){
+        close(listen_fd);
+        return -1;
+    }
+
+    return listen_fd;
 }
 
 
--- makefile
+++ makefile
@@ -6,11 +6,16 @@ LIBS 	:= -lpthread
 INCLUDE := 
 CXXFLAGS:= -std=c++11 -g -Wall -o3 $(INCLUDE)
 
-test: $(obj)
-	$(CC) $(source) $(CXXFLAGS) -o $@ $(obj) $(LIBS)
+all : $(obj)
+
+main.o : main.cpp 
+	$(CC) -c main.cpp $(CXXFLAGS) $(LIBS)
+epoll.o : epoll.cpp epoll.h
+	$(CC) -c epoll.cpp $(CXXFLAGS) $(LIBS)
+util.o : util.h util.cpp
+	$(CC) -c util.cpp $(CXXFLAGS) $(LIBS)
 
 #$(obj): $(source)
-#	$(CC) $(CXXFLAGS) -MMH -c $< -o $@
 
 .PHONY : clean
 clean :
