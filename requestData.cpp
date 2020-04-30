77bf8edca823e3639e38fc41b613aec21d6aab2b
root <root@localhost.localdomain>
2020-04-30 21:05:30 +0800

    事件处理 改bug

--- requestData.cpp
+++ requestData.cpp
@@ -117,8 +117,60 @@ void RequestData::handleRead()
     if(!errno)
     do
     {
+        int read_sum = readn(fd, inBuffer);
+        //read errno
+        if(read_sum < 0){
+            perror("readn error!");
+            errno = true;
+            handleError(fd, 400, "Bad Request");
+            break;
+        }
+        else if(read_sum == 0){
+            errno = true;
+            break;
+        }
+        //success ready to next
+        if(state == STATE_PARSE_URL)
+        {
+
+            
+        }
+        if(state == STATE_PARSE_HEADERS)
+        {
+
+        }
+        if(state == STATE_RECV_BODY)
+        {
+
+        }
 
     } while(false);
+    if(errno){
+        //再次监听读
+        events |= EPOLLIN;
+
+    }
+    else{
+        if(outBuffer.size() > 0)
+        {
+            events |= EPOLLOUT;
+
+        }
+        if(state == STATE_FINISH)
+        {
+            if(keep_alive)
+            {
+                //长链接下继续监听read event
+                //需要重置读缓冲区 解析状态 上一次解析的内容
+                //read_pos 请求路径等
+                this -> reset();
+                events |= EPOLLIN;
+            }
+
+        }
+        else
+            events |= EPOLLIN;
+        }
 
 }
 
@@ -126,7 +178,15 @@ void RequestData::handleWrite()
 {
     if(!error)
     {
-
+        if(writen(fd, outBuffer) < 0)
+        {
+            perror("writen errno!");
+            events = 0;
+            error = true;
+        }
+        //缓冲区还有数据
+        else if(outBuffer.size() > 0)
+            events |= EPOLLOUT;
     }
 
 }
@@ -134,9 +194,50 @@ void RequestData::handleWrite()
 void RequestData::handleConn()
 {
 
-    if(!error)
+    if (!error)
     {
-
+        int timeout = 1000;
+        if(keep_alive) timeout *= 500;
+        //io完毕 初始化 通过分发函数进行重新判定
+        isAbleRead = false;
+        isAbleWrite = false;
+        //加入请求队列前加入定时器
+        Epoll::add_timer(shared_from_this(), timeout);
+        //判断当前事件状态是否需要写回
+        if((events & EPOLLIN) && (events & EPOLLOUT))
+        {
+            events = __uint32_t(0);
+            events |= EPOLLOUT;
+        }
+        else
+        {
+            events = __uint32_t(0);
+            events |= (EPOLLIN | EPOLLONESHOT);
+        }
+        events |= EPOLLET;
+        __uint32_t _events = events;
+        events = __uint32_t(0);
+        int res = Epoll::epoll_mod(fd, shared_from_this(), _events);
+        if(res < 0 )
+        {
+            printf("epoll_mod failed!");
+        }
+    }
+    else if (keep_alive)
+    {
+        isAbleRead = false;
+        isAbleWrite = false;
+        events = __uint32_t(0);
+        events |= (EPOLLIN | EPOLLET | EPOLLONESHOT);
+        int _events = events;
+        events = 0;
+        int timeout = 500 * 1000;
+        Epoll::add_timer(shared_from_this(), timeout); 
+        int res = Epoll::epoll_mod(fd, shared_from_this(), _events);
+        if(res < 0 )
+        {
+            printf("epoll_mod failed!");
+        }
     }
 }
 
